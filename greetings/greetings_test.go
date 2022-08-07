e62e5a3b5a4b32db0b9626c18a65d748e1ab1020
Eugene Rubanov <venikkin@gmail.com>
2022-08-07 18:08:53 +0100

    mod tutorial

---
+++ greetings/greetings_test.go
@@ -0,0 +1,24 @@
+package greetings
+
+import (
+	"regexp"
+	"testing"
+)
+
+// TestHelloName calls greetings.Hello with a name, checking
+// for valid return value.
+func TestHelloName(t *testing.T) {
+	name := "Gladys"
+	want := regexp.MustCompile(`\b` + name + `\b`)
+	msg, err := Hello(name)
+	if !want.MatchString(msg) || err != nil {
+		t.Fatalf(`Hello("Gladys") = %q, %v, want match for %#q, nil`, msg, err, want)
+	}
+}
+
+func TestHelloEmpty(t *testing.T) {
+	msg, err := Hello("")
+	if msg != "" || err == nil {
+		t.Fatalf(`Hello("") = %q, %v, want "", error`, msg, err)
+	}
+}
---
+++ hello/hello.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"log"
+)
+import "example.com/greetings"
+
+func main() {
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	names := []string{"Gladys", "Samantha", "Darrin"}
+
+	messages, err := greetings.Hellos(names)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(messages)
+}
